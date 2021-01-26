f6198bb22078a54404f7fc1387b476ad886bd797
yseyhan <yalcin.seyhan@hotmail.com>
2021-01-26 15:37:52 -0500

    movie 2 branch on the phone 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("Hello World,b21");
         System.out.println("B21 is learing GitHub");
         //mmmjj
+        //movie on the phone 10 min
     }
 
 }
