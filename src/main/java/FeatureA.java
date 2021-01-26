80eb073c6056d3a537ea35cefbf63377f3c10fab
yseyhan <yalcin.seyhan@hotmail.com>
2021-01-26 14:23:18 -0500

    Started working on a new branch and comment

--- src/main/java/FeatureA.java
+++ src/main/java/FeatureA.java
@@ -1,6 +1,7 @@
 public class FeatureA {
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
+        System.out.println("B21 is learning more");
 
     }
 }
